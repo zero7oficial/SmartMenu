44491deee5b68355a4fc57c2f753f4afafbbc8f8
Rodrigo D. Franz <rodriggofranz@gmail.com>
2017-11-27 17:04:11 -0200

    Commit Inicial

---
+++ system/language/english/upload_lang.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['upload_userfile_not_set'] 		= 'Ative para procurar por userfile.';
+$lang['upload_file_exceeds_limit'] 		= 'O tamanho do arquivo excede o limite do PHP.';
+$lang['upload_file_exceeds_form_limit'] = 'O upload excede o limite do formulário.';
+$lang['upload_file_partial'] 			= 'The file was only partially uploaded.';
+$lang['upload_no_temp_directory'] 		= 'The temporary folder is missing.';
+$lang['upload_unable_to_write_file'] 	= 'The file could not be written to disk.';
+$lang['upload_stopped_by_extension'] 	= 'The file upload was stopped by extension.';
+$lang['upload_no_file_selected'] 		= 'Nenhum arquivo selecionado.';
+$lang['upload_invalid_filetype'] 		= 'A extensão do Arquivo não está ativado para envio.';
+$lang['upload_invalid_filesize'] 		= 'O arquivo excede o tamanho permitido.';
+$lang['upload_invalid_dimensions'] 		= 'The image you are attempting to upload doesn\'t fit into the allowed dimensions.';
+$lang['upload_destination_error'] 		= 'A problem was encountered while attempting to move the uploaded file to the final destination.';
+$lang['upload_no_filepath'] 			= 'O local do arquivo não é válido.';
+$lang['upload_no_file_types']			= 'Você não especificou o tipo de arquivo.';
+$lang['upload_bad_filename'] 			= 'O nome desse arquivo já existe no servidor.';
+$lang['upload_not_writable'] 			= 'A pasta de uploads não pode ser reescrita.';
